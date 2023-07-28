e2961fa8ef2a13aca9b101ab12fa0e900528b444
paulbasuakabeya <paulbasuakabeya@gmail.com>
2023-07-28 17:07:52 +0100

    exercice number one

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
- // HELLO PAUL
+const nickName = Math.floor(Math.random()*3); 
+ 
+if (nickName = 0){
+    console.log('your people of God');
+} else if (nickName = 1)  {
+ console.log('hello people of God');
+}else{
+    console.log('welcom people of God');
+}
+
